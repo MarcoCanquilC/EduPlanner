73911efadb75194668431a9220ece4479495957d
MarcoCanquil <marco.canquil@sansano.usm.cl>
2024-11-22 12:55:57 -0300

    Creacion proyecto y app core

--- sitio/sitio/asgi.py
+++ sitio/sitio/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for EduPlanner project.
+ASGI config for sitio project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EduPlanner.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sitio.settings')
 
 application = get_asgi_application()
--- sitio/sitio/urls.py
+++ sitio/sitio/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for EduPlanner project.
+URL configuration for sitio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
