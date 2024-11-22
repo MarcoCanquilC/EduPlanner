327c3177a97a17fd19bdfc3c3312f4d16d59f304
MarcoCanquil <marco.canquil@sansano.usm.cl>
2024-11-22 17:52:13 -0300

    Creación de Eventos

--- sitio/core/admin.py
+++ sitio/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Evento
 
 # Register your models here.
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'tipo', 'fecha_inicio', 'fecha_fin')
+    list_filter = ('tipo',)
+    search_fields = ('titulo', 'descripcion')
--- sitio/core/models.py
+++ sitio/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Evento(models.Model):
+    TIPO_EVENTO = [
+        ('feriado', 'Feriado'),
+        ('examen', 'Examen'),
+        ('plazo', 'Plazo administrativo'),
+        ('actividad', 'Actividad general'),
+    ]
+
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True, null=True)
+    fecha_inicio = models.DateTimeField()
+    fecha_fin = models.DateTimeField()
+    tipo = models.CharField(max_length=20, choices=TIPO_EVENTO)
